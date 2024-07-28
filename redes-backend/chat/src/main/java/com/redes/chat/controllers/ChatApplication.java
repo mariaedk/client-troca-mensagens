d446155edd6a1cf73b98a44a16071395c467f34e
Luan Guarnieri <luanlg14022002@gmail.com>
2024-07-28 18:08:46 -0300

    ajustes

--- redes-backend/chat/src/main/java/com/redes/chat/controllers/ChatApplication.java
+++ redes-backend/chat/src/main/java/com/redes/chat/controllers/ChatApplication.java
@@ -2,8 +2,10 @@ package com.redes.chat.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.redes.chat.model.service"})
 public class ChatApplication {
 
 	public static void main(String[] args) {
